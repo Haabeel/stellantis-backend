16c47d2665dfeb83d28c811a4e64b11780642be0
Varun Pabreja <vpabreja@ucsd.edu>
2025-07-04 10:40:15 +0400

    changed routes for flask venv

--- app.py
+++ app.py
@@ -582,4 +582,4 @@ def get_job_history():
             conn.close()
 
 if __name__ == "__main__":
-    app.run(debug=True,  use_reloader=False, threaded=False)
+    app.run(debug=True, port = 4001, use_reloader=False, threaded=False)
